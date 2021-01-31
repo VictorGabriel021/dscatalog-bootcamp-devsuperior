a3412daf45822ccd2c3b4fc7767c790f8c7a47ce
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2021-01-31 16:41:13 -0300

    Add products details

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
+import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
 
 const Routes = () => (
@@ -10,13 +11,16 @@ const Routes = () => (
    <Navbar />
    <Switch>
      <Route path="/" exact>
-      <Home />
+       <Home />
      </Route>
-     <Route path="/catalog">
-      <Catalog />
+     <Route path="/products" exact>
+       <Catalog />
+     </Route>
+     <Route path="/products/:productId">
+       <ProductDetails />
      </Route>
      <Route path="/admin">
-      <Admin />
+       <Admin />
      </Route>
    </Switch>
   </BrowserRouter>
