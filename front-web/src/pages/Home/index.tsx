d8b845c646f32a16565f480be949476ddf0f3411
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2021-02-02 17:20:30 -0300

    Finish catalog loaders

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
-import ButtonIcon from '../../core/components/Buttonicon';
+import { ReactComponent as MainImage } from 'core/assets/images/main-image.svg';
+import ButtonIcon from 'core/components/Buttonicon';
 import './styles.scss';
 
 const Home = () => (
