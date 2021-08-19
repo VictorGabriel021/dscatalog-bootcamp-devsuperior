4979391cee28989b7078e1d7a4b688c2c35d2ed2
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2021-08-19 17:11:50 -0300

    Make login and save user session data

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,36 +1,54 @@
 import ButtonIcon from 'core/components/Buttonicon';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import AuthCard from '../Card';
 import { useForm } from 'react-hook-form';
 import './styles.scss';
+import { makeLogin } from 'core/utils/request';
+import { useState } from 'react';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
 const Login = () => {
     const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHastError] = useState(false);
+    const history = useHistory();
 
     const onSubmit = (data: FormData) => {
-        console.log(data);  
+        makeLogin(data)
+            .then(response => {
+                setHastError(false);
+                saveSessionData(response.data);
+                history.push('/admin');
+            })
+            .catch(() => {
+                setHastError(true);
+            })
     }
 
     return (
         <AuthCard title="Login">
+            {hasError && (
+                <div className="alert alert-danger mt-5">
+                    Usuário ou senha inválidos!
+                </div>
+            )}
             <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
                     className="form-control input-base margin-bottom-30"
                     placeholder="Email"
-                    {...register('email')}
+                    {...register('username', {required: true})}
                 />
                 <input
                     type="password"
                     className="form-control input-base"
                     placeholder="Senha"
-                    {...register('password')}
+                    {...register('password', {required: true})}
                 />
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha?
