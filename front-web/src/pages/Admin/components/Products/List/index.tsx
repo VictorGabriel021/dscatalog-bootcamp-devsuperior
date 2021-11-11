9fff5f2c0aa648016bf6e5c2f65b1ed47f21b93a
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2021-11-11 11:09:12 -0300

    Remove product

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,8 +1,9 @@
 import Pagination from 'core/components/Pagination';
 import { ProductsResponse } from 'core/types/Product';
-import { makeRequest } from 'core/utils/request';
-import { useEffect, useState } from 'react';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
+import { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import Card from '../Card';
 
 const List = () => {
@@ -11,7 +12,7 @@ const List = () => {
   const [activePage, setActivePage] = useState(0);
   const history = useHistory();
 
-  useEffect(() => {
+  const getProducts = useCallback(() => {
     const params = {
       page: activePage,
       linesPerPage: 4,
@@ -26,8 +27,27 @@ const List = () => {
       })
   }, [activePage]);
 
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
   const handleCreate = () => {
     history.push('/admin/products/create');
+  }
+
+  const onRemove = (productId: number) => {
+    const confirm = window.confirm('Deseja realmente excluir este produto?');
+
+    if (confirm) {
+      makePrivateRequest({ url: `/products/${productId}`, method: 'DELETE' })
+        .then(() => {
+          toast.info('Produto removido com sucesso!');
+          getProducts();
+        })
+        .catch(() => {
+          toast.error('Erro ao remover o produto!');
+        })
+    }
   }
 
   return (
@@ -37,7 +57,7 @@ const List = () => {
       </button>
       <div className="admin-list-container">
         {productsResponse?.content.map(product => (
-          <Card product={product} key={product.id} />
+          <Card product={product} key={product.id} onRemove={onRemove} />
         ))}
         {productsResponse && (
           <Pagination
